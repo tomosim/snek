ddf8cee1ce0a02f0acc9f6e29970abee3d9b7806
Tom <TomSimmons@Toms-MacBook-Pro.local>
2018-11-06 11:56:57 +0000

    start and stop functions added. start button added

--- src/App.js
+++ src/App.js
@@ -3,14 +3,30 @@ import Grid from "./Components/Grid";
 import "./App.css";
 
 class App extends Component {
+  state = { timer: null, playing: false };
   render() {
     return (
       <div className="App">
         <div id="title">snek2</div>
-        <Grid />
+        <Grid
+          snek={this.state.snekArr}
+          playing={this.state.playing}
+          play={this.play}
+          stop={this.stop}
+        />
       </div>
     );
   }
+
+  play = func => {
+    const timer = setInterval(func, 200);
+    this.setState({ timer, playing: true });
+  };
+
+  stop = () => {
+    clearInterval(this.state.timer);
+    this.setState({ playing: false });
+  };
 }
 
 export default App;
--- src/Components/Grid.js
+++ src/Components/Grid.js
@@ -1,24 +1,109 @@
 import React, { PureComponent } from "react";
 
 class Grid extends PureComponent {
-  state = { gridArr: [] };
+  state = {
+    gridArr: [],
+    snekArr: [{ x: 9, y: 9 }]
+  };
 
   componentDidMount() {
     const gridArr = [];
     for (let y = 0; y < 20; y++) {
       for (let x = 0; x < 20; x++) {
-        gridArr.push(<div className={`grid-cell`} id="" key={`${x},${y}`} />);
+        gridArr.push(
+          <div
+            className={`grid-cell`}
+            id={
+              this.state.snekArr.find(cell => cell.x === x && cell.y === y)
+                ? "snekked"
+                : ""
+            }
+            key={`${x},${y}`}
+          />
+        );
       }
     }
-
     this.setState({
       gridArr
     });
   }
 
   render() {
-    return <div id="grid">{this.state.gridArr}</div>;
+    return (
+      <div id="grid">
+        {this.state.gridArr}
+        {this.props.playing || (
+          <button id="start" onClick={() => this.props.play(this.moveSnek)}>
+            START
+          </button>
+        )}
+      </div>
+    );
   }
+
+  moveSnek = (direction = "right") => {
+    const newSnekArr = [...this.state.snekArr];
+    newSnekArr.shift();
+
+    if (direction === "right") {
+      newSnekArr.push({
+        x:
+          this.state.snekArr[this.state.snekArr.length - 1].x + 1 > 19
+            ? 0
+            : this.state.snekArr[this.state.snekArr.length - 1].x + 1,
+        y: this.state.snekArr[this.state.snekArr.length - 1].y
+      });
+    }
+    if (direction === "left") {
+      newSnekArr.push({
+        x:
+          this.state.snekArr[this.state.snekArr.length - 1].x - 1 < 0
+            ? 19
+            : this.state.snekArr[this.state.snekArr.length - 1].x - 1,
+        y: this.state.snekArr[this.state.snekArr.length - 1].y
+      });
+    }
+    if (direction === "up") {
+      newSnekArr.push({
+        y:
+          this.state.snekArr[this.state.snekArr.length - 1].y - 1 < 0
+            ? 19
+            : this.state.snekArr[this.state.snekArr.length - 1].y - 1,
+        x: this.state.snekArr[this.state.snekArr.length - 1].x
+      });
+    }
+    if (direction === "down") {
+      newSnekArr.push({
+        y:
+          this.state.snekArr[this.state.snekArr.length - 1].y + 1 > 19
+            ? 0
+            : this.state.snekArr[this.state.snekArr.length - 1].y + 1,
+        x: this.state.snekArr[this.state.snekArr.length - 1].x
+      });
+    }
+
+    const gridArr = [];
+    for (let y = 0; y < 20; y++) {
+      for (let x = 0; x < 20; x++) {
+        gridArr.push(
+          <div
+            className={`grid-cell`}
+            id={
+              newSnekArr.find(cell => cell.x === x && cell.y === y)
+                ? "snekked"
+                : ""
+            }
+            key={`${x},${y}`}
+          />
+        );
+      }
+    }
+
+    this.setState({
+      snekArr: newSnekArr,
+      gridArr
+    });
+  };
 }
 
 export default Grid;
